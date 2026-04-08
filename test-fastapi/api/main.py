3d1e2ddc11a5d6dba9f1676d271d9eb98047aa0a
Tanjib-Rafi <rafitanjibulhasan@gmail.com>
2026-04-08 23:15:14 +0600

    fix

--- test-fastapi/api/main.py
+++ test-fastapi/api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from api.routers import traffic
+from api.database import Base, engine
 
 app = FastAPI()
 
 app.include_router(traffic.router)
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
--- test-fastapi/models/models.py
+++ test-fastapi/models/models.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, Float, String, TIMESTAMP
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/test-fastapi"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
